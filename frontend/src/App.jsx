b9c4a2ab41f995cb68b20c41074cee9156fe1eab
mohamedilyes <mohamedilyes.bouguerri@gmail.com>
2024-09-28 15:44:47 +0100

    the initial structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,31 @@
 import './App.css';
 import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Product from './sections/Product';
+import Category from './sections/Category';
+import Home from './sections/Home' ;
+import Chart from './sections/Chart';
+import Login from './sections/Login';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
+      <Header/>
         <Routes>
-          <Route path='' element={}/>
-          <Route path='' element={}/>
-          <Route path='' element={}/>
-          <Route path='' element={}/>
-          <Route path='' element={}/>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/men' element={<Category/>}/>
+          <Route path='/women' element={<Category/>}/>
+          <Route path='/kids' element={<Category/>}/>
+          <Route path='/product' element={<Product/>}>
+            <Route path=':productid' element={<Product/>}/>
+          </Route>
+          <Route path='/chart' element={<Chart/>}/>
+          <Route path='/login' element={<Login/>}/>
         </Routes>
+      <Footer/>
       </BrowserRouter>
     </>
   )
