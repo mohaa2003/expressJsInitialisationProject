9360b654f41ae83b3619ead617187e66c81011e9
mohamedilyes <mohamedilyes.bouguerri@gmail.com>
2024-10-29 17:26:40 +0100

    Completing the popular product section

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -1,10 +1,23 @@
+import {Link} from "react-router-dom";
+import { IoSearch } from "react-icons/io5";
+import "./item.css";
+
 const Item = ({id,name,oldPrice,newPrice,imagePath}) => {
   return (
-    <div>
-        <img src={imagePath} width={300} alt={`product_${id}`} />
-        <h3>{name}</h3>
-        <h3>{oldPrice}</h3>
-        <h3>{newPrice}</h3>
+    <div className="shadow-md shadow-slate-500 rounded-lg">
+        <div className="relative conte w-full group overflow-hidden">
+            <img className="group-hover:scale-125 transition-all duration-500 w-full" src={imagePath} alt={`product_${id}`} />
+            <Link className="link absolute w-[50px] h-[50px] rounded-full bg-white flex justify-center scale-0 items-center group-hover:scale-100 hover:rotate-[-270deg] transition-all duration-500 group " to={`product/${id}`}>
+                <IoSearch className=""/>
+            </Link>
+        </div>
+        <div className="p-4 flex flex-col gap-3">
+            <h4 className="line-clamp-2 font-medium text-sm text-center text-gray-800">{name}</h4>
+            <div className="flex justify-around items-center">
+                <p className="text-sm font-medium text-green-500">{newPrice} DZD</p>
+                <p className="text-sm font-medium text-red-500 line-through">{oldPrice} DZD</p>
+            </div>
+        </div>
     </div>
   )
 }
--- frontend/src/sections/Popular.jsx
+++ frontend/src/sections/Popular.jsx
@@ -4,9 +4,15 @@ import Item from "../components/Item";
 const Popular = () => {
   return (
     <section>
-        {data.slice(0,10).map((product,index)=>(
-            <Item key={index} id={product.id} name={product.name} oldPrice={product.oldPrice} newPrice={product.newPrice} imagePath={product.imagePath}/>
-        ))}
+        <div>
+            <h3 className="text-center pt-6 pb-4 font-bold text-3xl">Popular Products</h3>
+            <hr className="h-[3px] w-1/2 mx-auto bg-gradient-to-l from-transparent via-black to-transparent" />
+        </div>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-8 gap-y-12 p-6">
+            {data.slice(0,10).map((product,index)=>(
+                <Item key={index} id={product.id} name={product.name} oldPrice={product.oldPrice} newPrice={product.newPrice} imagePath={product.imagePath}/>
+            ))}
+        </div>
     </section>
   )
 }
